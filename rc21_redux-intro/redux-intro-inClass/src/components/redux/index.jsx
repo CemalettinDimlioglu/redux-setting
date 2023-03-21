ab6adaf93d135940c9c0f399b5102583640d3a0b
CMLTTN <cemaldim@gmail.com>
2023-03-21 10:14:14 +0300

    index

---
+++ rc21_redux-intro/redux-intro-inClass/src/components/redux/index.jsx
@@ -0,0 +1,16 @@
+ 
+ const initialState = {
+
+     counter:0
+ }
+ export const  reducer =(state= initialState, action)=>{
+     
+switch (key) {
+     case value:
+          
+          break;
+
+     default:
+          break;
+}
+ }
