e4a706532fe24898d6dbe7f04d86cc8b0d25d8a5
CMLTTN <cemaldim@gmail.com>
2023-03-21 17:56:38 +0300

    app-index

--- rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
+++ rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
@@ -21,7 +21,7 @@ import counterReducer from "./reducers/counterReducer";
 import todoReducer from "./reducers/todoReducer";
 
 const rootReducer = combineReducers({
-  counter:counterReducer,
+  count:counterReducer,
   todo:todoReducer,
 })
 export const store = createStore(rootReducer)
