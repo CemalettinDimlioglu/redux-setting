40699dfcb771d2eb5f461ef48240d461c595e35c
CMLTTN <cemaldim@gmail.com>
2023-03-21 11:36:50 +0300

    inc-dec-res

--- rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
+++ rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
@@ -1,16 +1,16 @@
- 
- const initialState = {
+const initialState = {
+  counter: 0,
+};
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREASE":
+      return { counter: state.counter + 1 };
+    case "DECREASE":
+      return { counter: state.counter - 1 };
+    case "RESET":
+      return { counter: 0 };
 
-     counter:0
- }
- export const  reducer =(state= initialState, action)=>{
-
-switch (action.type) {
-     case "INCREASE":
-       return {counter:state.counter +1}   
-
-
-     default:
-          break;
-}
- }
+    default:
+      return state;
+  }
+};
