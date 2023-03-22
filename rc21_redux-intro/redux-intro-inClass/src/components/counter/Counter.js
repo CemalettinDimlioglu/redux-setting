3bca6d14023b7bccabb5e8bffff19adc050a8039
CMLTTN <cemaldim@gmail.com>
2023-03-22 09:12:55 +0300

    work

--- rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
+++ rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
@@ -1,20 +1,39 @@
 import "./Counter.css";
 import { useDispatch, useSelector } from "react-redux";
-
+import { dec, inc, reset } from "../../redux/actions/counterAction";
 
 const Counter = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   // const counter = useSelector((state)=>state.counter)
   // const counter = useSelector((state)=>state.count.counter)
-  const {counter} = useSelector((state)=>state.count)
+  const { counter } = useSelector((state) => state.count);
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
       <h1>counter:{counter}</h1>
       <div>
-        <button className="counter-button positive"onClick={()=>dispatch({type: "INCREASE"})}>increase</button>
-        <button className="counter-button zero"onClick={()=>dispatch({type: "RESET"})}>reset</button>
-        <button className="counter-button negative"onClick={()=>dispatch({type: "DECREASE"})}>decrease</button>
+        <button
+          className="counter-button positive"
+          // onClick={() => dispatch({ type: "INCREASE" })}
+          onClick={() => dispatch(inc())}
+        >
+          increase
+        </button>
+        <button
+          className="counter-button zero"
+          // onClick={() => dispatch({ type: "RESET" })}
+          onClick={() => dispatch(reset())}
+        >
+          reset
+        </button>
+        <button
+          className="counter-button negative"
+          // onClick={() => dispatch({ type: "DECREASE" })}
+
+          onClick={() => dispatch(dec())}
+        >
+          decrease
+        </button>
       </div>
     </div>
   );
